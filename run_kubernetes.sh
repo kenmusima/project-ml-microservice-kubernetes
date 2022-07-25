216cf88b2d8dca04f67520f9fe1cdb11c1877ca9
kenmusima <ksmusima@gmail.com>
2022-07-25 12:00:40 +0300

    Changes to readme, outputing prediction.

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath="kensi72/mlmicroserviceapp"
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run mlmicroserviceapp\
+    --image="$dockerpath"
+    --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward mlmicroserviceapp 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,12 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath="kensi72/mlmicroserviceapp"
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-
+docker login &&\
+    docker image tag kensi72/mlmicroserviceapp $dockerpath
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
